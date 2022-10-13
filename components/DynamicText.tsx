fd517acbb9ba19dcf080c9386cd728cf0309d146
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-10-13 16:03:46 +0900

    Feat able to change text throug refHook

--- components/DynamicText.tsx
+++ components/DynamicText.tsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, forwardRef, useImperativeHandle } from "react";
 import { Heading } from "@chakra-ui/react";
 
-const DynamicText = () => {
+const DynamicText = forwardRef((props, ref) => {
   const [value, setValue] = useState("Random Text");
-
   const changeValue = (newValue) => {
     setValue(newValue);
   };
+
+  useImperativeHandle(ref, () => ({
+    changeValue,
+  }));
 
   return (
     <Heading as="h2" size="xl">
       {value}
     </Heading>
   );
-};
+});
 
 export default DynamicText;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,12 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import DynamicText from "../components/DynamicText";
 import { Input } from "@chakra-ui/react";
+import React, { useRef } from "react";
 const Home = () => {
-  const onChange = (e) => {
-    console.log(e.target.value);
+  const textRef = useRef(null);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    textRef.current?.changeValue(e.target.value);
   };
 
   return (
@@ -14,7 +17,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <DynamicText />
+        <DynamicText ref={textRef} />
         <Input variant="flushed" placeholder="random word" width="auto" onChange={onChange} />
       </main>
     </div>
