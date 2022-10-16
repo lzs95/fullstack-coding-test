24f74f30121afa873fcb671a1156b274c869f54f
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-10-16 19:51:14 +0900

    Fix refactoring of blog website comp

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import { Box, Center, Heading, Text, Stack, Avatar, useColorModeValue } from "@chakra-ui/react";
+import { Box, Center, Heading, Text, Stack, Avatar, useColorModeValue, Grid, GridItem } from "@chakra-ui/react";
 import { getFirestore, collection, getDocs, onSnapshot } from "firebase/firestore";
+import Navbar from "../components/Navbar";
 import { useEffect, useState } from "react";
 
 export default function blogPostWithImage() {
@@ -21,63 +22,41 @@ export default function blogPostWithImage() {
     });
   };
 
-  const displayData = () => {
-    console.log("here");
-    blogPost.map((post) => {
-      console.log(post);
-      return (
-        <Box maxW={"445px"} w={"full"} boxShadow={"2xl"} rounded={"md"} p={6} overflow={"hidden"}>
-          <Box h={"210px"} bg={"gray.100"} mt={-6} mx={-6} mb={6} pos={"relative"}>
-            <Image layout={"fill"} src={post["blog-image"]} />
-          </Box>
-          <Stack>
-            <Text color={"green.500"} textTransform={"uppercase"} fontWeight={800} fontSize={"sm"} letterSpacing={1.1}>
-              Blog
-            </Text>
-            <Heading fontSize={"2xl"} fontFamily={"body"}>
-              Boost your conversion rate
-            </Heading>
-            <Text>FUUCK</Text>
-            <Text color={"gray.500"}>{post["blog-content"]}</Text>
-          </Stack>
-        </Box>
-      );
-    });
-  };
-
   useEffect(() => {
     getAllData();
-    // displayData();
   }, []);
 
   return (
-    <Center py={6}>
-      {blogPost.map((post) => {
-        console.log(post);
-        return (
-          <Box
-            key={post["id"]}
-            gap={10}
-            maxW={"445px"}
-            w={"full"}
-            boxShadow={"2xl"}
-            rounded={"md"}
-            p={6}
-            overflow={"hidden"}>
-            <Box h={"210px"} bg={"gray.100"} mt={-6} mx={-6} mb={6} pos={"relative"}>
-              {/* <Image layout={"fill"} src={post["blog-image"]} /> */}
-            </Box>
-            <Stack>
-              <Heading fontSize={"2xl"} color={"green.500"} fontFamily={"body"}>
-                {post["blog-title"]}
-              </Heading>
-              <Text color={"gray.500"} noOfLines={5}>
-                {post["blog-content"]}
-              </Text>
-            </Stack>
-          </Box>
-        );
-      })}
-    </Center>
+    <>
+      <Navbar />
+      <Center py={6}>
+        <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+          {blogPost.map((post) => {
+            return (
+              <GridItem
+                key={post["id"]}
+                gap={10}
+                maxW={"445px"}
+                boxShadow={"2xl"}
+                rounded={"md"}
+                p={6}
+                overflow={"hidden"}>
+                <Box h={"210px"} bg={"gray.100"} mt={-6} mx={-6} mb={6} pos={"relative"}>
+                  <Image layout={"fill"} src={post["blog-image"]} />
+                </Box>
+                <Stack>
+                  <Heading fontSize={"2xl"} color={"green.500"} fontFamily={"body"}>
+                    {post["blog-title"]}
+                  </Heading>
+                  <Text color={"gray.500"} noOfLines={5}>
+                    {post["blog-content"]}
+                  </Text>
+                </Stack>
+              </GridItem>
+            );
+          })}
+        </Grid>
+      </Center>
+    </>
   );
 }
